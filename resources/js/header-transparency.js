5cd35df92ab01114b212a584cfef07d35f76c668
Cory Monteleone <cmontele@rcgc.edu>
2018-03-20 15:17:45 -0400

    Last commit of day

--- resources/js/header-transparency.js
+++ resources/js/header-transparency.js
@@ -1,3 +1,3 @@
 $(document).scroll(function() {
-    console.log("We go scrollin, scrollin, scrollin, scrollin, scrollin")
+    if()
 });
--- resources/js/no-vid-mobile.js
+++ resources/js/no-vid-mobile.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     if ($(window).width() < 768) {
         $(".dt-only").remove();
-    } 
+    }
+
+    $(document).scroll(function() {
+        if ($("body").hasClass("fp-viewing-Home") === false) {
+            $("#pill").addClass('hidden');
+        } else if ($("body").hasClass("fp-viewing-Home")) {
+            $("#pill").removeClass('hidden');
+        }
+    });
 });
